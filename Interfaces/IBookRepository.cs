167db26e51aef554609893479e1727354bce476a
DamianOrzechowski <72198238+DamianOrzechowski@users.noreply.github.com>
2022-03-03 22:52:29 +0100

    first commit

---
+++ Interfaces/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LibApp.Models;
+
+namespace LibApp.Interfaces
+{
+    public interface IBookRepository
+    {
+        public void AddBook(Book book);
+
+        public void UpdateBook(Book book);
+
+        public void DeleteBook(int id);
+
+        public IEnumerable<Book> GetBooks();
+
+        public Book GetBookById(int id);
+
+        void Save();
+    }
+}
